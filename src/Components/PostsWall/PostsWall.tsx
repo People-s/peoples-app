c78be665222b1d00a45fbcbe013b3854f6aff176
vidvidvid <weetopol@gmail.com>
2022-03-27 12:25:13 +0200

    feat: set up secret post button

--- src/Components/PostsWall/PostsWall.tsx
+++ src/Components/PostsWall/PostsWall.tsx
@@ -4,8 +4,9 @@ import {
   Avatar,
   Button,
   useDisclosure,
+  HStack,
+  Text,
   VStack,
-  Text
 } from "@chakra-ui/react";
 import { FC, useContext, useEffect, useState } from "react";
 
@@ -14,44 +15,75 @@ import CreatePostDialog from "../CreatePostDialog/CreatePostDialog";
 import AppNetworkContext from "../AppNetworksContext/AppNetworkContext";
 
 interface PostWallProps {
-  channel: any
+  channel: any;
 }
 
-const PostWall: FC<PostWallProps> = ({ channel: { attributes: { profileId: channelId } } }) => {
+const PostWall: FC<PostWallProps> = ({
+  channel: {
+    attributes: { profileId: channelId },
+  },
+}) => {
   const { createPost, getPosts } = useContext(AppNetworkContext);
-  const { isOpen: isCreateOpen, onOpen: onCreateOpen, onClose: onCreateClose } = useDisclosure();
-  const [posts, setPosts] = useState<any[]>([])
+  const {
+    isOpen: isCreateOpen,
+    onOpen: onCreateOpen,
+    onClose: onCreateClose,
+  } = useDisclosure();
+  const [posts, setPosts] = useState<any[]>([]);
   const sendPostCreate = (text: string) => {
-    createPost(channelId, text)
-  }
+    createPost(channelId, text);
+  };
   useEffect(() => {
     async function receivePosts() {
       const receivedPosts = await getPosts();
-      const filteredPosts = receivedPosts.filter(post => {
-        return post.attributes.profileId === channelId
-      })
+      const filteredPosts = receivedPosts.filter((post) => {
+        return post.attributes.profileId === channelId;
+      });
       setPosts(filteredPosts);
     }
-    receivePosts()
+    receivePosts();
   }, []);
-  
-  return (<>
-    <Flex dir="row" alignItems="center" mb={4}>
-      <Avatar
-        cursor="pointer"
-        size="md"
-        name="Ada Lovelace"
-        mr={6}
-        bgColor="gray"
-      />
-      <VStack width={'100%'}>
-        <Button size="md" width={'100%'} colorScheme="blue" variant="outline" onClick={onCreateOpen}>Click here to write your post</Button>
-        <Text mt={0} fontSize='sm'>Remember! You need to follow this channel first!</Text>
+
+  return (
+    <>
+      <VStack dir="row" alignItems="center" mb={4}>
+        <HStack justifyContent="space-between" width="100%">
+          <Avatar
+            cursor="pointer"
+            size="md"
+            name="Ada Lovelace"
+            mr={6}
+            bgColor="gray"
+          />
+
+          <Flex>
+            <Button
+              size="md"
+              mr={2}
+              colorScheme="blue"
+              variant="outline"
+              onClick={onCreateOpen}
+            >
+              Create a post
+            </Button>
+            <Button
+              size="md"
+              colorScheme="blue"
+              variant="outline"
+              onClick={onCreateOpen}
+            >
+              Create a secret post 😉
+            </Button>
+          </Flex>
+        </HStack>
+        <Box w="100%">
+          <Text mt={0} fontSize="sm" textAlign="end">
+            Remember! You need to follow this channel first!
+          </Text>
+        </Box>
       </VStack>
-    </Flex>
-    <Box pl={-1} pr={-1} pb={-1} overflow="auto" height="80vh">
-      {posts.map(
-        ({ id, attributes: { contentURI, transaction_index } }) => {
+      <Box pl={-1} pr={-1} pb={-1} overflow="auto" height="80vh">
+        {posts.map(({ id, attributes: { contentURI, transaction_index } }) => {
           return (
             <Post
               key={id}
@@ -61,11 +93,15 @@ const PostWall: FC<PostWallProps> = ({ channel: { attributes: { profileId: chann
               coin={"MATIC"}
             />
           );
-        }
-      )}
-    </Box>
-    <CreatePostDialog isOpen={isCreateOpen} onOpen={onCreateOpen} onClose={onCreateClose} onPostCreate={sendPostCreate} />
-  </>
+        })}
+      </Box>
+      <CreatePostDialog
+        isOpen={isCreateOpen}
+        onOpen={onCreateOpen}
+        onClose={onCreateClose}
+        onPostCreate={sendPostCreate}
+      />
+    </>
   );
 };
 
